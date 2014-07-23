24cbab8f8c47ce121a9f5e8f423aac5405903449
Basanta Kharel <basanta078@gmail.com>
2014-07-23 18:17:21 -0400

    Included the screens alex had from before
    Included the screens from before but now in angular js app folder.

--- frontend/com/example/drsmoothie/app/scripts/app.js
+++ frontend/com/example/drsmoothie/app/scripts/app.js
@@ -23,9 +23,9 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
+      .when('/smoothie', {
+        templateUrl: 'views/smoothieList.html',
+        controller: 'SmoothieListCtrl'
       })
       .otherwise({
         redirectTo: '/'
--- frontend/com/example/drsmoothie/app/scripts/controllers/smoothie.js
+++ frontend/com/example/drsmoothie/app/scripts/controllers/smoothie.js
@@ -8,7 +8,7 @@
  * Controller of the comdrsmoothieappApp
  */
 angular.module('comdrsmoothieappApp')
-  .controller('AboutCtrl', function ($scope) {
+  .controller('SmoothieListCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
