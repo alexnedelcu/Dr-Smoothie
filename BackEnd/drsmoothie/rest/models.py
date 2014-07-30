87e72986d929fb1ee9d33bc410d2e308a2b8e7af
Alexandru Nedelcu <ialexandru.nedelcu@gmail.com>
2014-07-30 14:15:08 -0400

    Create a view that simulates the many to many relation between the ingredients and nutrients

--- BackEnd/drsmoothie/rest/models.py
+++ BackEnd/drsmoothie/rest/models.py
@@ -2,10 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Nutrient(models.Model):
+    id_nutrient = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     
 class Ingredient(models.Model):
+    id_ingredient = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
-    nutrients = models.CharField(max_length=100)
     
+class NutrIngrMap(models.Model):
+    id_nutrient = models.ForeignKey(Nutrient)
+    id_ingredient = models.ForeignKey(Ingredient)
     
+#test:
+#Orange -   1
+#Lemon -    2
+#Apple -    3
+
+#vitamin C    1
--- BackEnd/drsmoothie/rest/views.py
+++ BackEnd/drsmoothie/rest/views.py
@@ -1,10 +1,24 @@
 from django.http import HttpResponse
 from models import *
 from django.core import serializers
+import json
 # Create your views here.
 
 
 def ingredient(request):
     ingredients = Ingredient.objects.all()
+    
     return HttpResponse(serializers.serialize("json", ingredients))
-#    return HttpResponse("Should be replaced by the ingredients JSON")
+
+def nutrients_of_an_ingredient(request):
+    nutrient = Nutrient.objects.get(pk=request.GET['id'])
+    
+    related_ingredients = NutrIngrMap.objects.filter(id_nutrient__exact = nutrient);
+    
+    ingredients = list()
+    for ingr in related_ingredients:
+        ingredients.append(ingr.id_ingredient)
+    #ingredients = Ingredient.objects.filter(id_ingredient__in = related_ingredients)
+    
+    return HttpResponse(serializers.serialize("json", ingredients))
+#    return HttpResponse(json.dumps(related_ingredients))
