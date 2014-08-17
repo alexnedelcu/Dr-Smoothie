eaa9d5d47415573e11708586cd7f3ff4c0f66800
Basanta Kharel <basanta078@gmail.com>
2014-08-17 15:22:14 -0400

    changed app id to dr-smoothie. debug = true and add dr-smoothie.appspot.com to allowed hosts.

--- BackEnd/drsmoothie/settings.py
+++ BackEnd/drsmoothie/settings.py
@@ -20,7 +20,9 @@ AUTOLOAD_SITECONF = 'indexes'
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dr-smoothie.appspot.com']
+
+DEBUG = True
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
